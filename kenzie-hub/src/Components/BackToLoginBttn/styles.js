3b8cf81bfab063714217dbd21e7472db66014fc5
Gabryel <gabryelmaraujo@gmail.com>
2022-11-30 12:03:13 -0300

    feat: initial styles create

---
+++ kenzie-hub/src/Components/BackToLoginBttn/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+import BackToLoginBttn from "./index"
+
+const BackToLoginBttnStyles = styled.div`
+
+    .backToLoginBttn{
+        height: 2.5rem;
+        padding: 0 1rem;
+
+        background-color: var(--grey-3);
+        color: var(--grey-0);
+
+        border: 1px solid var(--grey-3);
+        border-radius: 0.25rem;
+    }
+
+`
+
+export default BackToLoginBttnStyles
