5c6a8c3a7377f1399b4888561d8f39dc734abb9b
Gabryel <gabryelmaraujo@gmail.com>
2022-12-02 15:40:13 -0300

    feat: register function //without: toastify because lib error

--- kenzie-hub/src/Components/RegisterForm/index.jsx
+++ kenzie-hub/src/Components/RegisterForm/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
+// import { toast, ToastContainer } from "react-toastify/dist/components";
+// import "react-toastify/dist/ReactToastify.css";
+
 import * as yup from "yup"
-import { appendErrors, useForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup" 
 
 import RegisterButton from "../RegisterBttn";
 import StyledForm from "./styles";
+
+import instance from "../../data/api";
 
 const RegisterForm = ({newUser, setNewUser}) => {
   
@@ -15,15 +20,53 @@ const RegisterForm = ({newUser, setNewUser}) => {
     password: yup.string().required("É obrigatório uma senha!").matches(/(?=.*?[A-Z])/, "É necessário, pelo menos uma letra maíuscula").matches(/(?=.*?[a-z])/, "É necessário pelo menos uma letra minúscula.").matches(/(?=.*?[0-9])/, "É necessário, pelo menos um número.").matches(/(?=.*?[#?!@$%^&*-])/, "É necessário pelo menos um caractere special").min(8, "A senha precisa ter no mínimo 8 caracteres."),
     confirmPass: yup.string().oneOf([yup.ref('password'), null], 'As senhas têm que coincidirem.'),
     bio: yup.string().required("Fale um pouco sobre você!"),
-    contact: yup.string().required("Informe um número para contato!"),
+    contact: yup.string().required("Informe um contato!"),
     course_module: yup.string().required("Informe o seu módulo!")
   })
 
   const {register, handleSubmit, formState: {errors}} = useForm({ resolver: yupResolver(formSchema) })
 
+  async function registerFunction(formData){
+
+    // function registerToast(result){
+    //   if(result === "success"){
+    //     toast.success("Você foi cadastrado!", {
+    //       position: toast.POSITION.TOP_RIGHT
+    //     })
+    //   }else if(result === "emailError"){
+    //     toast.error("Esse email já esta sendo usado!", {
+    //       position: toast.POSITION.TOP_RIGHT
+    //     })
+    //   }
+    // }
+
+    try{
+
+      const response = await instance.post("users", formData)
+
+      if(response.status === 201){
+        // registerToast("success")
+        console.log("registrou")
+
+        setTimeout(() => {
+          console.log("aqui redireciona")
+        }, 2000);
+      }
+
+    }catch(error){
+      if(error.response.data.message === "Email already exists"){
+        console.log("Email já existe")
+      }
+    }finally{
+      console.log("loading")
+    }
+  }
+
   function formSubmiter(data){
 
-    postTreat(data)
+    const treatedData = postTreat(data)
+
+    registerFunction(treatedData)
 
   }
 
